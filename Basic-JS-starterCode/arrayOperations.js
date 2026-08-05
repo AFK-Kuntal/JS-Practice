becd9241f51e04264078296962408b73317517ab
Kuntal <kuntalpanda1@gmail.com>
2026-08-06 02:11:54 +0530

    spread operator

---
+++ Basic-JS-starterCode/arrayOperations.js
@@ -0,0 +1,44 @@
+const restaurant = {
+name: 'Classico Italiano',
+location: 'Via Angelo Tavanti 23, Firenze, Italy',
+categories: ['Italjan', 'Pizzeria', 'Vegetarian',
+'Organic'],
+starterMenu: ['Focaccia', 'Bruschetta', 'GarlicBread', 'Caprese Salad'],
+mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+};
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+[main, secondary] = [secondary, main];
+console.log("after swapping", main, secondary);
+let arr = [3, 5, [8, 9]];
+let [x, , [y, z]] = arr;
+console.log(x, y, z);
+//setting default values
+const [a = 1, b = 1, c = 1] = [10, 20];
+console.log(a, b, c); //although there isn't a third value in the array c will have a default value of 1
+
+//Spread Operator
+/*
+You can use the JavaScript spread operator (...) to unpack elements from an iterable (like an array or object,maps, sets etc) into situations where multiple arguments, elements, or variables are expected.
+*/
+ arr = [7, 8, 9];
+const newArr = [1, 2, ...arr]; //[1, 2, arr[0], arr[1], arr[2]] appends the values;
+console.log(newArr); //prints the entire array
+console.log(...newArr) // prints individual values
+anotherArr = [1, 2, newArr];
+console.log(anotherArr); //inserts the entire array as an element
+joinedArr = [...newArr, ...anotherArr];
+console.log(joinedArr);
+
+const str = 'Jonas';
+const letters = [...str, ' ', 'S. '];
+console.log(letters);
+const newRestaurent = {
+    founded : 2000,
+    ...restaurant.categories,
+    founder : 'XYZ',
+    ...restaurant,
+};
+console.log(newRestaurent);
