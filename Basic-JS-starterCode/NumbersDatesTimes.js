9a4b28654903d56a8e40cf9e9d0b07c6a7bbb1a5
Kuntal <kuntalpanda1@gmail.com>
2026-08-28 02:15:05 +0530

    Math Functions

--- Basic-JS-starterCode/NumbersDatesTimes.js
+++ Basic-JS-starterCode/NumbersDatesTimes.js
@@ -24,3 +24,33 @@ console.log(Number.isFinite(parseFloat("sejfio34")));
 console.log(Number.isInteger(23));
 console.log(Number.isInteger(23.0));
 console.log(Number.isInteger(23.005546));
+
+//Math
+console.log(Math.sqrt(25));
+console.log(9 ** (1 / 2));
+console.log(27 * (1 / 3));
+console.log(Math.max(10, 23, 0, 4));
+console.log(Math.min(10, 23, 0, 4));
+const arr = [2, 4, 6, 7,2,5];
+console.log(Math.max(...arr));
+console.log(Math.max(...arr) * Math.PI);
+//trunc --> cuts decimal part
+//round --> nearest integer
+//ceil --> higher integer
+//floor --> lower integer
+console.log(Math.floor("23")); //rounding methods supports type coercion
+console.log(Math.trunc(Math.max(...arr) * Math.PI));
+console.log(Math.round(Math.max(...arr) * Math.PI));
+console.log(Math.ceil(Math.max(...arr) * Math.PI));
+console.log(Math.floor(Math.max(...arr) * Math.PI));
+console.log(Number((Math.max(...arr) * Math.PI).toFixed(2))); //toFixed generates string
+
+
+//bigInt --> Primitive Datatype
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER); //after this js suffers to store 
+console.log(3412345823457892345763284569827356729385); //can't represent
+console.log(3412345823457892345763284569827356729385n);
+console.log(typeof 3412345823457892345763284569827356729385n);
+// console.log(10n * 12) //not possible
+console.log(BigInt(23687364)); //Bigint works well with comperatively smaller number than 'n'
